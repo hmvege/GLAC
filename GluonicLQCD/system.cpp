5f384ebc22c7fc05404817ee16612db1ff66c5c2
hmvege <hmvege@ulrik.uio.no>
2017-09-18 08:22:26 +0200

    Testing with scalar field config...

--- GluonicLQCD/system.cpp
+++ GluonicLQCD/system.cpp
@@ -219,8 +219,15 @@ void System::update()
 
 void System::runMetropolis(bool storePreObservables, bool writeConfigsToFile)
 {
-//    loadFieldConfiguration("input/beta6.000000_config0.bin");
-//    exit(1);
+    MPI_Barrier(MPI_COMM_WORLD);
+    loadFieldConfiguration("../../config1.bin");
+    MPI_Barrier(MPI_COMM_WORLD);
+    double corr = m_correlator->calculate(m_lattice);
+    MPI_Allreduce(&corr, &corr, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    corr /= double(m_numprocs);
+    if (m_processRank == 0) cout << "Plaquette value: " << corr << endl << endl;
+    MPI_Barrier(MPI_COMM_WORLD);
+    exit(1);
     // Variables for checking performance of the update.
     clock_t preUpdate, postUpdate;
     double updateStorer = 0;
