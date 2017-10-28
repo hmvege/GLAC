92b92bceac692f946b1662d54728f770af8422b7
hmvege <hmvege@ulrik.uio.no>
2017-10-28 17:58:30 +0200

    Bug seems to be related to the value of the update step. If it is equal or less than 1e-7 it is okay.

--- GluonicLQCD/flow/flow.cpp
+++ GluonicLQCD/flow/flow.cpp
@@ -34,15 +34,16 @@ void Flow::flowGaugeField(int NFlows, Links *lattice)
         runFlow(lattice);
     }
 
-    // Tests if flow preserves the SU3 matrix properties
+    // Tests if flow preserves the SU3 matrix properties ===================
     TestSuite test;
     for (unsigned int i = 0; i < m_subLatticeSize; i++) {
         for (unsigned int mu = 0; mu < 4; mu++) {
             test.testMatrix(lattice[i].U[mu],true);
             exit(1);
         }
-
     }
+    // =====================================================================
+
 }
 
 void Flow::runFlow(Links *lattice)
@@ -69,9 +70,9 @@ SU3 Flow::exponentiate(SU3 Q)
     /*
      * Takes the exponential of an Hermitian matrix.
      */
-    // Makes Q hermitian. MOVE TO FUNCTION?
     I.identity();
 
+    // Makes Q hermitian. MOVE TO FUNCTION?
     double temp = 0;
     for (int i = 0; i < 9; i++) {
         temp = Q.mat[2*i];
@@ -81,6 +82,7 @@ SU3 Flow::exponentiate(SU3 Q)
 
     QSquared = Q*Q;
     QCubed = Q*QSquared;
+
 
 //    std::cout << "Q: " << std::endl;
 //    Q.print();
@@ -124,24 +126,20 @@ SU3 Flow::exponentiate(SU3 Q)
     h[1].setIm(2*u*sinu*cosw + 2*u*sin2u + xi0*(3*uu - ww)*cosu);
     h[2].setRe(-3*u*xi0*sinu - cosu*cosw + cos2u);
     h[2].setIm(-3*u*xi0*cosu + sinu*cosw + sin2u);
-//    h[0].setRe((u*u - w*w)*cos(2*w) + 8*u*u*cos(u)*cos(w) + 2*u*(3*u*u + w*w)*sin(w)*xi0); // SIMPLIFY w*w, sin(w), cos(w), cos(2*u), sin(2*u), 3*u*u - w*w
-//    h[0].setIm((u*u - w*w)*sin(2*u) + cos(u)*2*u*(3*u*u + w*w)*xi0 - sin(w)*8*u*u*cos(w));
-//    h[1].setRe(2*u*cos(2*u) - cos(u)*2*u*cos(w) - sin(u)*(3*u*u - w*w)*xi0);
-//    h[1].setIm(2*u*sin(2*u) + cos(u)*(3*u*u - w*w)*xi0 - sin(u)*2*u*cos(w));
-//    h[2].setRe(cos(2*u) - cos(u)*cos(w) - sin(u)*3*u*xi0);
-//    h[2].setIm(sin(2*u) - cos(u)*3*u*xi0 + sin(u)*cos(w));
 
     // Sets f
     for (int i = 0; i < 3; i++) {
         f[i] = h[i] / (9*uu - ww);
-        if (c0 < 0) {
-            f[0] = f[0].c();
-            f[1] = f[1].c()*(-1);
-            f[2] = f[2].c();
+    }
+
+    // Checks for negative c0 coefficient
+    if (c0 < 0) {
+        for (int i = 0; i < 3; i++) {
+            f[i] = f[i].c()*pow(-1,double(i));
         }
     }
 
-    // Testing if we uphold the identity Q^3 - c1*Q - c0*I = 0
+    // Testing if we uphold the identity Q^3 - c1*Q - c0*I = 0 =============
     SU3 X = QCubed - Q*c1 - I*c0;
     for (int i = 0; i < 18; i++) {
         if (X.mat[i] > 1e-16) {
@@ -151,11 +149,12 @@ SU3 Flow::exponentiate(SU3 Q)
             exit(1);
         }
     }
-//    exit(1);
-//    I.setComplex(f[0],0);
-//    I.setComplex(f[0],8);
-//    I.setComplex(f[0],16);
-    return I*f[0] + Q*f[1] + QSquared*f[1];
+    // =====================================================================
+
+    I.setComplex(f[0],0);
+    I.setComplex(f[0],8);
+    I.setComplex(f[0],16);
+    return I + Q*f[1] + QSquared*f[1];
 }
 
 void Flow::smearLink(Links *lattice, unsigned int i, unsigned int j, unsigned int k, unsigned int l, int mu)
@@ -165,17 +164,43 @@ void Flow::smearLink(Links *lattice, unsigned int i, unsigned int j, unsigned in
      */
     // Sets first RK3 constant, W0
     W[0] = lattice[m_Index->getIndex(i,j,k,l)].U[mu]; // V should be the previous flowed point!
+
+    // TEST ================================================================
+    TestSuite test;
+    std::cout << "MATRIX W0" << std::endl;
+    test.testMatrix(W[0],true);
+    // =====================================================================
+
     // Finds Z0
     Z[0] = m_S->getActionDerivative(lattice,W[0],i,j,k,l,mu) * m_epsilon;
     // Sets second RK3 constant, W1
     W[1] = exponentiate(Z[0] * 0.25) * W[0];
+
+    // TEST ================================================================
+    std::cout << "MATRIX W1" << std::endl;
+    test.testMatrix(W[1],true);
+    exit(1);
+    // =====================================================================
+
     // Finds Z1
     Z[1] = m_S->getActionDerivative(lattice,W[1],i,j,k,l,mu) * m_epsilon;
     // Sets third RK3 constant, W2
     W[2] = exponentiate(Z[1]*0.8888888888888888 - Z[0]*0.4722222222222222) * W[1];
+
+    // TEST ================================================================
+    std::cout << "MATRIX W2" << std::endl;
+    test.testMatrix(W[2],true);
+    // =====================================================================
+
     // Sets the new, flowed SU3 matrix.
     m_updatedLattice[m_Index->getIndex(i,j,k,l)].U[mu].copy(exponentiate(m_S->getActionDerivative(lattice,W[2],i,j,k,l,mu)*m_epsilon*0.75 - Z[1]*0.8888888888888888 + Z[0]*0.4722222222222222)*W[2]);
     // HOW MUCH MEMORY THAT I WILL USE: (64**3*128*4*18*8)/1024/1024/1024*2 / (256/16)
+
+    // TEST ================================================================
+    std::cout << "MATRIX W3" << std::endl;
+    test.testMatrix(m_updatedLattice[m_Index->getIndex(i,j,k,l)].U[mu],true);
+    std::cout << "Reached end of smear link" << std::endl;
+    // =====================================================================
 }
 
 void Flow::setIndexHandler(IndexOrganiser *Index)
