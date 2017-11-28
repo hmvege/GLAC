c23c4c28b467532ee783cee4c3320f02f574dab7
hmvege <hmvege@ulrik.uio.no>
2017-11-28 15:51:49 +0100

    added some comments in the derivative.

--- GluonicLQCD/actions/wilsongaugeaction.cpp
+++ GluonicLQCD/actions/wilsongaugeaction.cpp
@@ -49,73 +49,11 @@ void WilsonGaugeAction::computeStaple(Links *lattice, unsigned int i, unsigned i
 
 SU3 WilsonGaugeAction::getActionDerivative(Links * lattice, unsigned int i, unsigned int j, unsigned int k, unsigned int l, int mu)
 {
+    // Computes the staple for current link
     computeStaple(lattice,i,j,k,l,mu);
-//    m_staple = lattice[Parallel::Index::getIndex(i,j,k,l)].U[mu]*m_staple; // My method
 
-//    if (Parallel::Communicator::getProcessRank() == 0) {
-//        printf("\n");
-//        m_staple.printMachine();
-//    }
-
-//    m_staple.zeros();
-//    updateMuIndex(mu);
-//    m_position[0] = i;
-//    m_position[1] = j;
-//    m_position[2] = k;
-//    m_position[3] = l;
-//    for (int nu = 0; nu < 4; nu++)
-//    {
-//        if (mu == nu) continue;
-//        updateNuIndex(nu);
-//        // Getting first part of staple
-//        m_staple1 = lattice[Parallel::Index::getIndex(i,j,k,l)].U[nu];
-//        m_staple1 *= Parallel::Communicator::getPositiveLink(lattice,m_position,nu,m_nuIndex,mu);
-//        m_staple1 *= Parallel::Communicator::getPositiveLink(lattice,m_position,mu,m_muIndex,nu).inv();
-//        // Getting second part of staple
-//        m_staple2 = Parallel::Communicator::getNegativeLink(lattice,m_position,nu,m_nuIndex,nu).inv();
-//        m_staple2 *= Parallel::Communicator::getNegativeLink(lattice,m_position,nu,m_nuIndex,mu);
-//        m_staple2 *= Parallel::Communicator::getNeighboursNeighbourLink(lattice,m_position,mu,m_muIndex,nu,m_nuIndex,nu);
-//        // Sums staple
-//        m_staple += m_staple1;
-//        m_staple += m_staple2;
-//    }
-//    m_staple = lattice[Parallel::Index::getIndex(i,j,k,l)].U[mu]*m_staple.inv(); // Morningstar method, but staple has wrong orientation
-
-//    Parallel::Communicator::setBarrier();
-//    if (Parallel::Communicator::getProcessRank() == 0) {
-//        printf("\n");
-//        m_staple.inv().printMachine();
-//    }
-//    Parallel::Communicator::setBarrier();
-//    Parallel::Communicator::MPIExit("EXITS");
-
-    // How should on take the staple?
+    // Multiplying staple together with link
     m_staple = lattice[Parallel::Index::getIndex(i,j,k,l)].U[mu]*m_staple; // My method
-//    m_staple *= lattice[Parallel::Index::getIndex(i,j,k,l)].U[mu];
-//    m_staple = lattice[Parallel::Index::getIndex(i,j,k,l)].U[mu].inv()*m_staple.inv();
-//    m_staple = m_staple.inv()*lattice[Parallel::Index::getIndex(i,j,k,l)].U[mu].inv(); // Chroma
-//    m_staple = m_staple.inv()*lattice[Parallel::Index::getIndex(i,j,k,l)].U[mu];
-//    m_staple = m_staple*lattice[Parallel::Index::getIndex(i,j,k,l)].U[mu].inv();
-
-
-//    // Derivative test
-//    m_staple.setComplex(complex(1,1),0);
-//    m_staple.setComplex(complex(1,2),2);
-//    m_staple.setComplex(complex(1,3),4);
-//    m_staple.setComplex(complex(2,1),6);
-//    m_staple.setComplex(complex(2,2),8);
-//    m_staple.setComplex(complex(2,3),10);
-//    m_staple.setComplex(complex(3,1),12);
-//    m_staple.setComplex(complex(3,2),14);
-//    m_staple.setComplex(complex(3,3),16);
-//    Parallel::Communicator::setBarrier();
-//    if (Parallel::Communicator::getProcessRank() == 0) {
-//        printf("\n");
-//        m_staple.printMachine();
-//    }
-//    Parallel::Communicator::setBarrier();
-
-    // When first flowed topc is 4.51027660073873, I have the correct method
 
     // MORNINGSTAR METHOD
     Omega = m_staple.inv();
@@ -126,14 +64,6 @@ SU3 WilsonGaugeAction::getActionDerivative(Links * lattice, unsigned int i, unsi
     }
     m_X = Omega*0.5;
 
-//    Parallel::Communicator::setBarrier();
-//    if (Parallel::Communicator::getProcessRank() == 0) {
-//        printf("\n");
-//        m_X.printMachine();
-//    }
-//    Parallel::Communicator::setBarrier();
-//    Parallel::Communicator::MPIExit("EXITS");
-
     /////// FIX LUSHCER METHOD ///////
     // LUSCHER METHOD
     // Multiply the product of this with each of the 8 T^a generators
@@ -141,28 +71,6 @@ SU3 WilsonGaugeAction::getActionDerivative(Links * lattice, unsigned int i, unsi
     // Multiply with 8 generators T^a again, sum and return matrix
     // Can multiply the return matrix exactly instead of doing the 8 matrix multiplications and sums ect. See python script gellmann_matrix_multiplication.py.
 //    // Diagonals
-//    m_X.mat[0] = 0;
-//    m_X.mat[1] = (m_staple.mat[9] - 2*m_staple.mat[1] + m_staple.mat[17])*0.1666666666666666;
-//    m_X.mat[8] = 0;
-//    m_X.mat[9] = (m_staple.mat[1] - 2*m_staple.mat[9] + m_staple.mat[17])*0.1666666666666666;
-//    m_X.mat[16] = 0;
-//    m_X.mat[17] = (m_staple.mat[1] + m_staple.mat[9] - 2*m_staple.mat[17])*0.1666666666666666;
-//    // Upper triangular
-//    m_X.mat[2] =    0.25 * (m_staple.mat[6]  - m_staple.mat[2]);
-//    m_X.mat[3] =  - 0.25 * (m_staple.mat[3]  + m_staple.mat[7]);
-//    m_X.mat[4] =    0.25 * (m_staple.mat[12] - m_staple.mat[4]);
-//    m_X.mat[5] =  - 0.25 * (m_staple.mat[5]  + m_staple.mat[13]);
-//    m_X.mat[10] =   0.25 * (m_staple.mat[14] - m_staple.mat[10]);
-//    m_X.mat[11] = - 0.25 * (m_staple.mat[11] + m_staple.mat[15]);
-//    // Lower triangular
-//    m_X.mat[6] = - m_X.mat[2];
-//    m_X.mat[7] = m_X.mat[3];
-//    m_X.mat[12] = - m_X.mat[4];
-//    m_X.mat[13] = m_X.mat[5];
-//    m_X.mat[14] = - m_X.mat[10];
-//    m_X.mat[15] = m_X.mat[11];
-
-//    // Diagonals // WITHOUT FACTOR 2
 //    m_X.mat[0] = 0;
 //    m_X.mat[1] = (m_staple.mat[9] - 2*m_staple.mat[1] + m_staple.mat[17])*0.3333333333333333;
 //    m_X.mat[8] = 0;
@@ -183,12 +91,6 @@ SU3 WilsonGaugeAction::getActionDerivative(Links * lattice, unsigned int i, unsi
 //    m_X.mat[13] = m_X.mat[5];
 //    m_X.mat[14] = - m_X.mat[10];
 //    m_X.mat[15] = m_X.mat[11];
-
-//    if (Parallel::Communicator::getProcessRank() == 0) {
-//        std::cout<<"LUSCHER:"<<std::endl;
-//        m_X.printMachine();
-//    }
-//    Parallel::Communicator::MPIExit("EXITS IN WILSONGAUGEACTIONDERIVATIVE");
 
     return m_X;
 }
