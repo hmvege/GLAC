c9fb8a92536fabd3d8a7c5163ddfc4b8327eddb2
Mathias <hmvege@ulrik.uio.no>
2017-12-05 13:59:08 +0100

    Found bug. Had changed the make-anti-hermitian method. Now seems to be flowing correctly.

--- GluonicLQCD/flow/flow.cpp
+++ GluonicLQCD/flow/flow.cpp
@@ -30,42 +30,15 @@ void Flow::flowField(Lattice<SU3> *lattice)
     /*
      * Performs a single flow on the lattice.
      */
-    printf("\nBEFORE ACTION DERIVATIVE\n");
-    lattice[0][0].print();
-    Parallel::Communicator::setBarrier();
     // W0 is simply just the original lattice times epsilon
     // Sets Z0 in temporary lattice
     for (unsigned int mu = 0; mu < 4; mu++) {
         m_tempLattice[mu] = m_S->getActionDerivative(lattice,mu);
     }
-    printf("\nAFTER ACTION DERIVATIVE\n");
-    Parallel::Communicator::setBarrier();
-    lattice[0][0].print();
-    Parallel::Communicator::setBarrier();
-    m_tempLattice[0][0].print();
-    Parallel::Communicator::setBarrier();
     // Sets W1 in main lattice
     for (unsigned int mu = 0; mu < 4; mu++) {
         lattice[mu] = matrixExp(m_tempLattice[mu]*(m_epsilon*0.25))*lattice[mu];
     }
-    printf("\nAFTER MATRIX EXPONENTIATION\n");
-    Parallel::Communicator::setBarrier();
-    lattice[0][0].print();
-    Parallel::Communicator::setBarrier();
-    m_tempLattice[0][0].print();
-    Parallel::Communicator::setBarrier();
-    exit(1);
-//    for (unsigned int x = 0; x < m_N[0]; x++) {
-//        for (unsigned int y = 0; y < m_N[1]; y++) {
-//            for (unsigned int z = 0; z < m_N[2]; z++) {
-//                for (unsigned int t = 0; t < m_N[3]; t++) {
-//                    for (unsigned int mu = 0; mu < 4; mu++) {
-//                        lattice[mu][Parallel::Index::getIndex(x,y,z,t)] = m_SU3ExpFunc->exp(m_tempLattice[mu][Parallel::Index::getIndex(x,y,z,t)]*m_epsilon*0.25)*lattice[mu][Parallel::Index::getIndex(x,y,z,t)];
-//                    }
-//                }
-//            }
-//        }
-//    }
     // Sets "Z1" in temporary lattice
     for (unsigned int mu = 0; mu < 4; mu++) {
         m_tempLattice[mu] = m_S->getActionDerivative(lattice,mu)*(m_epsilon*0.8888888888888888) - m_tempLattice[mu]*(m_epsilon*0.4722222222222222);
@@ -74,17 +47,6 @@ void Flow::flowField(Lattice<SU3> *lattice)
     for (unsigned int mu = 0; mu < 4; mu++) {
         lattice[mu] = matrixExp(m_tempLattice[mu])*lattice[mu];
     }
-//    for (unsigned int x = 0; x < m_N[0]; x++) {
-//        for (unsigned int y = 0; y < m_N[1]; y++) {
-//            for (unsigned int z = 0; z < m_N[2]; z++) {
-//                for (unsigned int t = 0; t < m_N[3]; t++) {
-//                    for (unsigned int mu = 0; mu < 4; mu++) {
-//                        lattice[mu][Parallel::Index::getIndex(x,y,z,t)] = m_SU3ExpFunc->exp(m_tempLattice[mu][Parallel::Index::getIndex(x,y,z,t)])*lattice[mu][Parallel::Index::getIndex(x,y,z,t)];
-//                    }
-//                }
-//            }
-//        }
-//    }
     // Sets "Z2" in temporary lattice
     for (unsigned int mu = 0; mu < 4; mu++) {
         m_tempLattice[mu] = m_S->getActionDerivative(lattice,mu)*(0.75*m_epsilon) - m_tempLattice[mu];
@@ -93,17 +55,6 @@ void Flow::flowField(Lattice<SU3> *lattice)
     for (unsigned int mu = 0; mu < 4; mu++) {
         lattice[mu] = matrixExp(m_tempLattice[mu])*lattice[mu];
     }
-//    for (unsigned int x = 0; x < m_N[0]; x++) {
-//        for (unsigned int y = 0; y < m_N[1]; y++) {
-//            for (unsigned int z = 0; z < m_N[2]; z++) {
-//                for (unsigned int t = 0; t < m_N[3]; t++) {
-//                    for (unsigned int mu = 0; mu < 4; mu++) {
-//                        lattice[mu][Parallel::Index::getIndex(x,y,z,t)] = m_SU3ExpFunc->exp(m_tempLattice[mu][Parallel::Index::getIndex(x,y,z,t)])*lattice[mu][Parallel::Index::getIndex(x,y,z,t)];
-//                    }
-//                }
-//            }
-//        }
-//    }
 }
 
 void Flow::setSU3ExpFunc()
@@ -128,6 +79,7 @@ void Flow::setSU3ExpFunc()
 
 Lattice<SU3> Flow::matrixExp(Lattice<SU3> lattice)
 {
+//    m_tempExpLattice.zeros();
     for (unsigned int ix = 0; ix < m_N[0]; ix++) {
         for (unsigned int iy = 0; iy < m_N[1]; iy++) {
             for (unsigned int iz = 0; iz < m_N[2]; iz++) {
