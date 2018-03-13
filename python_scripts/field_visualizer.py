e81f372c8c59ca55eada1cec912b824b880c2b05
Mathias <hmvege@ulrik.uio.no>
2018-03-13 16:22:33 -0400

    Added method for creating gifs and movies.

--- python_scripts/field_visualizer.py
+++ python_scripts/field_visualizer.py
@@ -1,14 +1,30 @@
 import numpy as np
 import os
 import itertools
-
-# import matplotlib.pyplot as plt
-# from mpl_toolkits.mplot3d import Axes3D
-# import scipy.interpolate as scp
-
-import traits
-
 from mayavi import mlab
+import subprocess
+
+def create_animation(figpath, observable, method, anim_type):
+	"""
+	Method for created gifs and movies from generated 3D figures.
+
+	Args:
+		figpath: path to figures.
+		observable: observable we are creating a gif or a movie for.
+		method: type of 3D plot.
+	"""
+	_ANIM_TYPES = ["gif", "mp4"]
+	assert anim_type in _ANIM_TYPES, "%s is not a recognized animation type." % anim_type
+	
+	output_path = os.path.join(figpath, '%s_%s.%s' % (observable, method, anim_type))
+	if anim_type == "gif":
+		input_paths = os.path.join(figpath, '%s_%s_t*.png' % (observable, method))
+		cmd = ['convert', '-delay', '1', '-loop', '0', input_paths, output_path]
+	else:
+		input_paths = os.path.join(figpath, '%s_%s_t%%02d.png' % (observable, method))
+		cmd = ['ffmpeg', '-framerate', '4', '-i', input_paths, '-c:v', 'libx264', '-r', '30', output_path]
+	subprocess.Popen(cmd, stdout=subprocess.PIPE)
+	print "%s animation %s created." % (anim_type, output_path)
 
 N = 8
 NT = 16
@@ -20,10 +36,6 @@ file_name = ("../output/lattice_field_density{0:<d}x{1:<d}/scalar_fields/"
 			 "{3:<s}/lattice_field_density{0:<d}x{1:<d}_"
 			 "b6.000000_N{0:<d}_NT{1:<d}_np{2:<d}_config0{4:<04d}.bin").format(N, NT, threads, observable, flowtime)
 
-# file_name = ("../output/lattice_field_density_test_run/field_configurations/"
-# 			 "lattice_field_density_test_run_topcflowLatticeDoublesField_beta6.000000_spatial8_temporal16_threads8_config01000.bin")
-
-
 def global_index(i, j, k, l):
 	return i + N*(j + N*(k + N*l)) # column-major
 
@@ -33,9 +45,6 @@ field = np.zeros((N, N, N, NT))
 file = np.fromfile(file_name)
 
 print "Retrieved file %s" % file_name
-# print file.shape
-# print np.sum(file) / lattice_size
-
 
 # with open(file_name, "rb") as f:
 # 	byte = f.read(1)
@@ -58,37 +67,9 @@ for it in xrange(NT):
 			for ix in xrange(N):
 				field[ix, iy, iz, it] = - file[global_index(ix, iy, iz, it)] / 64.0
 
+# field *= 1000
+
 print "Populated field."
-
-print field[:,:,:,0].shape
-# exit(1)
-# Plot scatter with mayavi
-# figure = mlab.figure('DensityPlot')
-# pts = mlab.points3d(field[:,:,:,0], scale_mode='none', scale_factor=0.07)
-
-
-#### Bad matplotlib method
-# fig = plt.figure()
-# ax = fig.gca(projection='3d')
-
-# for iz in xrange(N):
-# 	for iy in xrange(N):
-# 		for ix in xrange(N):
-# 			# field[ix, iy, iz, it] = file[global_index(ix, iy, iz, it)]
-# 			# try:
-# 			# print field[ix, iy, iz, 0]
-# 			ax.scatter(ix, iy, iz, c=field[ix, iy, iz, 0], marker="o", s=100, alpha=0.1, cmap="PRGn", edgecolor="0")
-# 			# except:
-# 			# 	print byte
-
-
-
-# source = mlab.pipeline.scalar_field(field[:,:,:,0])
-# vol = mlab.pipeline.volume(source)
-# mlab.figure()
-# mlab.test_contour3d()
-# mlab.contour3d(field[:,:,:,0])
-# mlab.test_plot3d()# (field[:,:,:,0])
 
 animation_figure_fpath = "../figures/act_density"
 
@@ -115,22 +96,21 @@ for ix in xrange(N):
 max_val = np.max(field)
 min_val = np.min(field)
 
-def plot_points3d(F, ftype, figpath):
+def plot_points3d(F, ftype, figpath, obs, vmin=None, vmax=None, cgif=True, cmovie=True):
 	mlab.figure()
 	for it in xrange(NT):
-		fpath = os.path.join(figpath, "act_dens_t%02d.%s" % (it, ftype))
-		mlab.points3d(F[:,:,:,it])
-		# mlab.savefig(fpath)
-		mlab.show()
-		# mlab.clf()
+		mlab.clf()
+		fpath = os.path.join(figpath, "%s_points3d_t%02d.%s" % (obs, it, ftype))
+		# mlab.points3d(F[:,:,:,it], vmin=vmin, vmax=vmax, scale_factor=100, scale_mode="scalar")
+		mlab.points3d(F[:,:,:,it], vmin=vmin, vmax=vmax)
+		mlab.savefig(fpath)
 		print "file created at %s" % fpath
+	mlab.close()
 
-# plot_points3d(field, file_type, animation_figure_fpath)
-
-# x,y,z = np.mgrid[0:N:1,0:N:1,0:N:1]
-
-# mlab.contour3d(scalar_array, vmax=max_val, vmin=min_val, transparent=True, contours=4)
-# # mlab.contour3d(x,y,z, scalar_array, vmax=max_val, vmin=min_val, transparent=True, contours=4)
+	if cgif:
+		create_animation(figpath, observable, "points3d", "gif")
+	if cmovie:
+		create_animation(figpath, observable, "points3d", "mp4")
 
 # Small routine for making program sleep
 import wx
@@ -141,28 +121,74 @@ def animate_sleep(x):
 		time.sleep(0.01)
 		wx.Yield()
 
-def plot_points3d(F, ftype, figpath):
+def plot_scalar_field(F, ftype, figpath, obs, anim=False, vmin=None, vmax=None, cgif=True, cmovie=True):
 	mlab.figure()
-	# source = mlab.pipeline.scalar_field(F[:,:,:,0])
-	# vol = mlab.pipeline.volume(source, vmin=min_val*2, vmax=max_val*0.8)
+	if anim:
+		source = mlab.pipeline.scalar_field(F[:,:,:,0])
+		vol = mlab.pipeline.volume(source, vmin=vmin, vmax=vmax)
+
+
+	# # Changing the ctf:
+	# from tvtk.util.ctf import ColorTransferFunction
+	# ctf = ColorTransferFunction()
+	# ctf.add_rgb_point(vmin, 0, 0, 1)  # r, g, and b are float
+	# ctf.add_rgb_point(vmax, 1, 0, 0)  # r, g, and b are float
+	#                                    # between 0 and 1
+	# # ...
+	# vol._volume_property.set_color(ctf)
+	# vol._ctf = ctf
+	# vol.update_ctf = True
+
+	# # Changing the otf:
+	# from tvtk.util.ctf import PiecewiseFunction
+	# otf = PiecewiseFunction()
+	# otf.add_point(vmin, 0)
+	# otf.add_point(vmax, 1)
+	# vol._otf = otf
+	# vol._volume_property.set_scalar_opacity(otf)
 
 	for it in xrange(1,NT):
+		fpath = os.path.join(figpath, "%s_volume_t%02d.%s" % (obs, it, ftype))
 		mlab.clf()
-		# animate_sleep(1)
-		fpath = os.path.join(figpath, "act_dens_t%02d.%s" % (it, ftype))
-		# vol.mlab_source.set(scalars=F[:,:,:,it])
+		
+		if anim:
+			animate_sleep(1)
+			vol.mlab_source.set(scalars=F[:,:,:,it], vmin=vmin, vmax=vmax)
+		else:
+			source = mlab.pipeline.scalar_field(F[:,:,:,it])
+			vol = mlab.pipeline.volume(source, vmin=vmin, vmax=vmax)
 
-		source = mlab.pipeline.scalar_field(F[:,:,:,it])
-		vol = mlab.pipeline.volume(source, vmin=min_val*2, vmax=max_val*0.8)
+			# # Changing the ctf:
+			# from tvtk.util.ctf import ColorTransferFunction
+			# ctf = ColorTransferFunction()
+			# ctf.add_rgb_point(vmin, 0, 0, 1)  # r, g, and b are float
+			# ctf.add_rgb_point(vmax, 1, 0, 0)  # r, g, and b are float
+			#                                    # between 0 and 1
+			# # ...
+			# vol._volume_property.set_color(ctf)
+			# vol._ctf = ctf
+			# vol.update_ctf = True
+
+			# # Changing the otf:
+			# from tvtk.util.ctf import PiecewiseFunction
+			# otf = PiecewiseFunction()
+			# otf.add_point(vmin, 0)
+			# otf.add_point(vmax, 1)
+			# vol._otf = otf
+			# vol._volume_property.set_scalar_opacity(otf)
+
 
 		mlab.savefig(fpath)
-		# mlab.show()
 		print "file created at %s" % fpath
 
 	mlab.close()
 
-plot_points3d(field, file_type, animation_figure_fpath)
-# mlab.show()
+	if cgif:
+		create_animation(figpath, observable, "volume", "gif")
+	if cmovie:
+		create_animation(figpath, observable, "volume", "mp4")
+
+plot_points3d(field, file_type, animation_figure_fpath, observable, vmin=min_val*1.5, vmax=max_val*0.9)
+# plot_scalar_field(field, file_type, animation_figure_fpath, observable, anim=False, vmin=min_val*1.5, vmax=max_val*0.9)
+
 print "Done"
-
-
